7decf307211e6a60baafaa9c373cbcf4ed33cf4f
Dmitry Tishchenko <flir3d@gmail.com>
2014-12-01 01:23:02 +0300

    Доделано составление словаря.

--- dictcomposer.py
+++ dictcomposer.py
@@ -8,7 +8,7 @@ import operator
 
 def compose_dictionary(filename, sentiment, path, count, dictsize):
     freqs = {}
-    for tweet in tweetsParser(filename, options.sentiment, options.mystempath, options.count):
+    for tweet in tweetsParser(filename, sentiment, path, count):
         unigrams = tweet.lemmatized
         print(unigrams)
         for word in unigrams:
@@ -19,9 +19,9 @@ def compose_dictionary(filename, sentiment, path, count, dictsize):
                 freqs[word] = freq + 1
     sorted_freqs = sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
     if dictsize !=0:
-        return sorted_freqs[:dictsize]
+        return dict(sorted_freqs[:dictsize])
     else:
-        return sorted_freqs
+        return dict(sorted_freqs)
 
 def intersect_dicts(positive_dict, negative_dict, pos_label="pos", neg_label="neg"):
     result = {}
@@ -32,15 +32,25 @@ def intersect_dicts(positive_dict, negative_dict, pos_label="pos", neg_label="ne
         result[pos_key] = pos_label
     for neg_key in neg_keys - intersection:
         result[neg_key] = neg_label
+    return result
 
+def main(pos_tweets_filename, neg_tweets_filename, tweets_count=float("inf"), mystempath = "./mystem", dictsize = 0,
+         pos_label="pos", neg_label="neg"):
+    pos_dict = compose_dictionary(pos_tweets_filename, pos_label, mystempath, tweets_count, dictsize)
+    neg_dict = compose_dictionary(neg_tweets_filename, neg_label, mystempath, tweets_count, dictsize)
+    return intersect_dicts(pos_dict, neg_dict, pos_label, neg_label)
 
 if __name__=="__main__":
     parser = OptionParser()
-    parser.add_option("-s", "--sentiment", dest="sentiment", default="pos")
     parser.add_option("-c", "--count",type='int',dest="count", default=float("inf"))
     parser.add_option("--dictsize",dest="dictsize",type='int', default=0)
     parser.add_option("--mystempath",dest="mystempath", default="./mystem")
-    parser.add_option("-f", "--filename",dest="filename")
+    parser.add_option("-p", "--postweets",dest="postweetsfilename")
+    parser.add_option("-n", "--negtweets",dest="negtweetsfilename")
     (options, args) = parser.parse_args()
-    unigrams_dict = compose_dictionary(options.filename, options.sentiment, options.mystempath, options.count, options.dictsize)
+    unigrams_dict = main(options.postweetsfilename,
+                         options.negtweetsfilename,
+                         options.count,
+                         options.mystempath,
+                         options.dictsize)
     sys.stdout.write(str(unigrams_dict))
